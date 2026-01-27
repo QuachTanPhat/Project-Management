243c8388f3175ae59c5032249becbf5f09e0ba4d
Dennis <qtphat060204.nvtroi1922@gmail.com>
2026-01-27 19:49:11 +0700

    fix inngest

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,17 +1,20 @@
+import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
-import { neon } from '@neondatabase/serverless'
+import { neonConfig } from '@neondatabase/serverless'
+import ws from 'ws'
+
+neonConfig.webSocketConstructor = ws
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is missing')
 }
 
-const sql = neon(process.env.DATABASE_URL)
-const adapter = new PrismaNeon(sql)
+const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaNeon({connectionString})
+const prisma = global.prisma || new PrismaClient({ adapter })
 
-const prisma = global.prisma ?? new PrismaClient({ adapter })
-
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'development') {
   global.prisma = prisma
 }
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'dotenv/config';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
@@ -8,10 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
-app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get('/', (req, res) => res.send('Server is running'));
 
+app.use("/api/inngest", serve({ client: inngest, functions }));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
