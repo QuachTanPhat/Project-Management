28bfd8b97218178a71355055ad93192ef89a36d5
Dennis <qtphat060204.nvtroi1922@gmail.com>
2026-01-27 18:23:24 +0700

    fix prismaed

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -4,15 +4,21 @@ import {Pool, neonConfig } from '@neondatabase/serverless';
 import ws from 'ws';
 if (process.env.NODE_ENV !== 'production') {
   const dotenv = await import('dotenv');
-  dotenv.config({ path: '../.env' }); // Hoặc đường dẫn đúng tới file .env của bạn
+  dotenv.config({ path: '../.env' }); 
 }
 neonConfig.webSocketConstructor = ws;
 
-
-
 const connectionString = process.env.DATABASE_URL;
+console.log("--------------- DEBUG CONNECTION ---------------");
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("DATABASE_URL status:", connectionString ? "✅ Đã tìm thấy" : "❌ Bị Null/Undefined");
+if (connectionString) {
+    console.log("DATABASE_URL length:", connectionString.length); // In độ dài để chắc chắn không phải chuỗi rỗng
+}
+console.log("------------------------------------------------");
+
 if (!connectionString) {
-  throw new Error("❌ LỖI: Không tìm thấy DATABASE_URL. Hãy kiểm tra Environment Variables trên Vercel!");
+  throw new Error("❌ CRITICAL ERROR: DATABASE_URL is missing. Please check Vercel Env Vars.");
 }
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
