4f52d4552d9e9fd750d90cfe1fb383a76729f438
Dennis <qtphat060204.nvtroi1922@gmail.com>
2026-01-27 18:38:13 +0700

    fix new prisma

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,29 +1,18 @@
-import { PrismaClient } from '@prisma/client';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import {Pool, neonConfig } from '@neondatabase/serverless';
-import ws from 'ws';
+import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { neon } from '@neondatabase/serverless'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is missing')
+}
+
+const sql = neon(process.env.DATABASE_URL)
+const adapter = new PrismaNeon(sql)
+
+const prisma = global.prisma ?? new PrismaClient({ adapter })
+
 if (process.env.NODE_ENV !== 'production') {
-  const dotenv = await import('dotenv');
-  dotenv.config({ path: '../.env' }); 
+  global.prisma = prisma
 }
-neonConfig.webSocketConstructor = ws;
 
-const connectionString = process.env.DATABASE_URL;
-console.log("--------------- DEBUG CONNECTION ---------------");
-console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("DATABASE_URL status:", connectionString ? "✅ Đã tìm thấy" : "❌ Bị Null/Undefined");
-if (connectionString) {
-    console.log("DATABASE_URL length:", connectionString.length); // In độ dài để chắc chắn không phải chuỗi rỗng
-}
-console.log("------------------------------------------------");
-
-if (!connectionString) {
-  throw new Error("❌ CRITICAL ERROR: DATABASE_URL is missing. Please check Vercel Env Vars.");
-}
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-const prisma = global.prisma || new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
-
-export default prisma;
+export default prisma
--- server/prisma.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@prisma/config';
-import 'dotenv/config';
-export default defineConfig({
-  datasources: {
-    url: process.env.DATABASE_URL,
-    directUrl: process.env.DIRECT_URL, 
-  },
-});
